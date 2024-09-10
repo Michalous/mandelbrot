feb5fdec5c318ab71bbe77658f3f7471cdad0896
Michalous <49964028+Michalous@users.noreply.github.com>
2024-09-10 22:15:12 +0100

    commit

--- zooming.js
+++ zooming.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext('2d')
 const coordsDisplay = document.getElementById('coords');
 const showZoom = document.getElementById('showZoom');
 
-let centerX = -0.75; // Initial center x-coordinate
+let centerX = -0.62; // Initial center x-coordinate
 let centerY = 0; // Initial center y-coordinate
 let rangeX = 2.8; // Initial range for x
 let rangeY = 2.5; // Initial range for y
@@ -53,14 +53,6 @@ canvas.addEventListener('click', function(evt) {
     rangeX /= 2; // Halve the range for x after each zoom
     rangeY /= 2; // Halve the range for y after each zoom
 
-    if (evt.button === 0) {
-        console.log('Left mouse button clicked');
-        console.log('Zoom level updated');
-    }
-    else if (evt.button === 2) {
-        console.log('Right mouse button clicked');
-        console.log('Zoom level updated');
-    }
     loopOverCanvas()
 });
 
@@ -69,9 +61,6 @@ canvas.addEventListener('contextmenu', function(evt) {
     evt.preventDefault(); // Prevent the context menu from appearing
     zoom--
     showZoom.textContent = zoom
-    if (evt.button === 2) { // Right button
-        console.log('hele')
-    }
     
     zoomOut(evt);
 });
@@ -85,26 +74,19 @@ function loopOverCanvas() {
             const newY = convertToNewRange(y, 0, canvas.height, centerY + rangeY / 2, centerY - rangeY / 2);
             const canvasX = convertToOldRange(newX, centerX - rangeX / 2, centerX + rangeX / 2, 0, canvas.width);
             const canvasY = convertToOldRange(newY, centerY + rangeY / 2, centerY - rangeY / 2, 0, canvas.height);
-            // Perform any operation with newX and newY
-            // console.log(`Converted Coordinates: (${newX}, ${newY})`);
-                drawDot(canvasX, canvasY, 1.25, divTest(newX, newY));
+            drawDot(canvasX, canvasY, 0.75, divTest(newX, newY));
             
         }
     }
 }
 
-// let coloursToDraw = [(66, 30, 15), (25, 7, 26), (9, 1, 47), (4, 4, 73), (0, 7, 100), (12, 44, 138), (24, 82, 177), (57, 125, 209), (134, 181, 229), (211, 236, 248), (241, 233, 191), (248, 201, 95), (255, 170, 0), (204, 128, 0), (153, 87, 0), (106, 52, 3)]
-// let coloursToDraw = [[66, 30, 15], [25, 7, 26], [9, 1, 47], [4, 4, 73], [0, 7, 100], [12, 44, 138], [24, 82, 177], [57, 125, 209], [134, 181, 229], [211, 236, 248], [241, 233, 191], [248, 201, 95], [255, 170, 0], [204, 128, 0], [153, 87, 0], [106, 52, 3]]
-let coloursToDraw = ['rgb(66, 30, 15)', 'rgb(25, 7, 26)', 'rgb(9, 1, 47)', 'rgb(4, 4, 73)', 'rgb(0, 7, 100)', 'rgb(12, 44, 138)', 'rgb(24, 82, 177)', 'rgb(57, 125, 209)', 'rgb(134, 181, 229)', 'rgb(211, 236, 248)', 'rgb(241, 233, 191)', 'rgb(248, 201, 95)', 'rgb(255, 170, 0)', 'rgb(204, 128, 0)', 'rgb(153, 87, 0)', 'rgb(106, 52, 3)']
-console.log(coloursToDraw[0])
 
 // Test of divergence
 function divTest(real, imaginary) {
-    // let coloursToDraw = [[66, 30, 15], [25, 7, 26], [9, 1, 47], [4, 4, 73], [0, 7, 100], [12, 44, 138], [24, 82, 177], [57, 125, 209], [134, 181, 229], [211, 236, 248], [241, 233, 191], [248, 201, 95], [255, 170, 0], [204, 128, 0], [153, 87, 0], [106, 52, 3]]
     let coloursToDraw = ['rgb(66, 30, 15)', 'rgb(25, 7, 26)', 'rgb(9, 1, 47)', 'rgb(4, 4, 73)', 'rgb(0, 7, 100)', 'rgb(12, 44, 138)', 'rgb(24, 82, 177)', 'rgb(57, 125, 209)', 'rgb(134, 181, 229)', 'rgb(211, 236, 248)', 'rgb(241, 233, 191)', 'rgb(248, 201, 95)', 'rgb(255, 170, 0)', 'rgb(204, 128, 0)', 'rgb(153, 87, 0)', 'rgb(106, 52, 3)']
 
     if ((real**2 + imaginary**2) > 4) {
-        return 0
+        return 'rgb(0, 0, 0)'
     }
     let z = new Complex(real, imaginary)
     let z_it = new Complex(0, 0)
